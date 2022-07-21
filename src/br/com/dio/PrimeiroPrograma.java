9afc467f4d96ebe25cc180f6fb0bd12f8f7651a1
CN-camargo <camilenayara3@gmail.com>
2022-07-21 18:06:02 -0300

    First commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,19 @@
+package br.com.dio;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+
+		Gato gato = new Gato();
+		Livros livros = new Livros();
+
+		// System.out.println("Olá, mundo!");
+
+	}
+
+}
+
+class Livros {
+	private String nome;
+	private String npag;
+}
